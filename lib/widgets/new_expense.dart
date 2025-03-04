c3b144dc09ec3d0225225fdd70bd2fd30076dd7c
bagoesrex <bagoesrex@gmail.com>
2025-03-05 05:39:06 +0700

    feat: implement dropdown categories

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -14,6 +14,7 @@ class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
   DateTime? _selectedDate;
+  Category _selectedCategory = Category.food;
 
   void _presentDatePicker() async {
     final now = DateTime.now();
@@ -80,18 +81,37 @@ class _NewExpenseState extends State<NewExpense> {
               ),
             ],
           ),
+          const SizedBox(height: 16),
           Row(
             children: [
+              DropdownButton(
+                value: _selectedCategory,
+                items:
+                    Category.values
+                        .map(
+                          (category) => DropdownMenuItem(
+                            value: category,
+                            child: Text(category.name.toUpperCase()),
+                          ),
+                        )
+                        .toList(),
+                onChanged: (value) {
+                  setState(() {
+                    if (value == null) {
+                      return;
+                    }
+                    _selectedCategory = value;
+                  });
+                },
+              ),
+              const Spacer(),
               TextButton(
                 onPressed: () {
                   Navigator.pop(context);
                 },
                 child: const Text('Cancel'),
               ),
-              ElevatedButton(
-                onPressed: () {},
-                child: Text('Simpan Pengeluaran'),
-              ),
+              ElevatedButton(onPressed: () {}, child: Text('Save Expense')),
             ],
           ),
         ],
