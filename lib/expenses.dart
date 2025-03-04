360d1e1ba31f1c15370e0ac597fc45af15122456
bagoesrex <bagoesrex@gmail.com>
2025-03-05 02:40:05 +0700

    feat: create expense model and dummy data

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:flow_fin/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -10,11 +11,18 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(title: 'Nasi Goreng', amount: 10, date: DateTime.now(), category: Category.food),
+    Expense(title: 'Bus', amount: 20, date: DateTime.now(), category: Category.transport),
+    Expense(title: 'Kebutuhan Bulanan', amount: 30, date: DateTime.now(), category: Category.shopping),
+    Expense(title: 'Bali', amount: 40, date: DateTime.now(), category: Category.travel),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: Column(
       children: const [
-        Text('Ini Chart'),
+        Text('Ini Chart'),  
         Text('Expenses List...')
       ],
     ),);
