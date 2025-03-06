bcd334fdea1d0458409c593fa8b7991daee5734c
bagoesrex <bagoesrex@gmail.com>
2025-03-07 04:11:16 +0700

    feat: adapt layout based on screen width

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flow_fin/widgets/expenses.dart';
+import 'package:flutter/services.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 46, 127, 177),
@@ -11,6 +12,10 @@ var kDarkColorScheme = ColorScheme.fromSeed(
 );
 
 void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((
+  //   fn,
+  // ) {
   runApp(
     MaterialApp(
       darkTheme: ThemeData.dark().copyWith(
@@ -22,7 +27,7 @@ void main() {
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
             backgroundColor: kDarkColorScheme.primaryContainer,
-            foregroundColor: kDarkColorScheme.onPrimaryContainer
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
           ),
         ),
       ),
@@ -53,4 +58,5 @@ void main() {
       home: const Expenses(),
     ),
   );
+  // });
 }
